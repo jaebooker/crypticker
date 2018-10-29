9c27d9b2fe6abf088ad4e654314ab75a0cf0b756
jaebooker <jaesonbooker@gmail.com>
2018-10-29 00:29:04 -0700

    added some elements to crypticker

---
+++ BTC Widget/TodayViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TodayViewController.swift
+//  BTC Widget
+//
+//  Created by Jaeson Booker on 10/28/18.
+//  Copyright © 2018 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+import CryptoCurrencyKit
+class TodayViewController: CurrencyDataViewController, NCWidgetProviding{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lineChartView.delegate = self
+        lineChartView.dataSource = self
+        
+        priceLabel.text = "--"
+        priceChangeLabel.text = "--"
+    }
+        
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        fetchPrices { error in
+            if error != nil {
+                self.updatePriceLabel()
+                self.updatePriceChangeLabel()
+                self.updatePriceHistoryLineChart()
+            }
+        }
+    }
+    
+}
